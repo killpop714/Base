e995d6d2964b6ef24cf2ab0412d53ece711045db
killpop714 <112853297+killpop714@users.noreply.github.com>
2024-11-04 12:03:18 +0900

    php에 대해서 알아가는 중

--- HDC.php
+++ HDC.php
@@ -1,5 +1,9 @@
 <?php
-    
+    $today = "title";
+    $value = "Hello World!!";
+
+    $value = $value."   Wow!!!! very good!!!";  // 정수형 타입의 $data  $data = 10;  // 사칙연산을 사용할 수 있습니다.  $data = $data + 1;
+    출처: https://nowonbun.tistory.com/574 [명월 일지:티스토리]
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,6 +13,6 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?=$today?>
 </body>
 </html>

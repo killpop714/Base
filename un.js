2cb613f42f032a43a45c7083b0b1468fff169743
killpop714 <112853297+killpop714@users.noreply.github.com>
2024-11-06 17:33:19 +0900

    데이터 옮기기

--- un.js
+++ un.js
@@ -1,10 +1,62 @@
+const num = Number(prompt("참가자는 몇명인가요?"));
+var check =0;
 
-var inputdata;
+const Andata = new Array(num);
 
-$input = document.getElementsByClassName("hell");
-console.log($input.value);
 
-function Output()
+const $input = document.querySelector('#i1');
+const $btn = document.querySelector('#Checkbtn');
+const $spam = document.querySelector("#spam");
+
+let data;
+let word;
+const inputdata = (e) =>
 {
+    data= e.target.value
 
+};
+
+    
+const Output = () =>
+ {
+    if(num != check)
+    {
+        if(!word)
+        {
+            $spam.textContent = word;
+            console.log("데이터가 저장되었습니다.");
+            console.log( $spam.textContent);
+            const order = Number($order.textContent);
+            if(order + 1>number){
+                $order.textContent = 1;
+            }
+            else
+            {
+                $order.textContent = order + 1;
+            }
+            check++;
+        }
+        else
+        {
+            if(word[word.length -1] === data[0])
+            {
+                $spam.textContent = word;
+                check++;
+            }
+            else
+            console.log("잘못된 값입니다.");
+        }
+                
+    }
+    else
+    {
+        console.log("데이터가 가득합니다.")
+    }
 }
+
+$input.addEventListener('input', inputdata);
+$btn.addEventListener('click',Output);
+
+Andata
+
+

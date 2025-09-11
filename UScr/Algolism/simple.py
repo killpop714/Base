8f507e5d5070455ff1a4421ed65fd00e58003b72
killpop714 <112853297+killpop714@users.noreply.github.com>
2025-09-11 14:24:52 +0900

    tnwjd

--- UScr/Algolism/simple.py
+++ UScr/Algolism/simple.py
@@ -1,8 +1,1 @@
-class Player:
-    def __init__(self,name,hp,attack):
-        self.name = name
-        self.hp = hp
-        self.attack = attack
-
-    def hit(self,target):
-        target.hp -= self.attack
+form ursina

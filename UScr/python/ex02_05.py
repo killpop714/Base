c4ab864000725f51c1489379e87a2296ad243ae5
killpop714 <112853297+killpop714@users.noreply.github.com>
2025-09-25 14:09:00 +0900

    도커 파일

---
+++ UScr/python/ex02_05.py
@@ -0,0 +1,18 @@
+print(7+5)
+print(7-5)
+print(7*5)
+print(7/5)
+print(10/5)
+
+b=10
+b= b+1#이퀄은 왼쪽 화살표
+print(b)
+
+num1 =7
+num2 = 5
+print(num1/num2)
+print(num1//num2)
+
+print(pow(num1,num2))
+
+str2 ="쿵덕"*2#문자열을 n개 만큼 반복함
